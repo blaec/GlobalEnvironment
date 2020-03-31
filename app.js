c6c56bbe3dd4f6f0088ceebbdec70546e9e57e0b
blaec <odeskonst@yandex.ru>
2020-03-31 21:59:19 +0300

    22. Operator Precedence and Associativity

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-// long running function
-function waitThreeSeconds() {
-    var ms = 3000 + new Date().getTime();
-    while (new Date() < ms) {}
-    console.log('finished function');
-}
-
-function clickHandler() {
-    console.log('click event!');
-}
-
-// listen for the click event
-document.addEventListener('click', clickHandler);
-
-waitThreeSeconds();
-console.log('finished execution');
+var a = 2, b = 3, c = 4;
+a = b = c;
+console.log(a);
+console.log(b);
+console.log(c);
