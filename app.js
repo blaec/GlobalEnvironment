5ec2791c71f8b1f10023652296dede4a9081de41
blaec <odeskonst@yandex.ru>
2020-04-03 23:19:39 +0300

    44. Immediately Invoked Functions Expressions (IIFEs)

--- app.js
+++ app.js
@@ -1,39 +1,6 @@
-console.log(this);      // global window
+var firstname = 'John';
 
-function a() {
-    console.log(this);  // global window
-    this.newvariable = 'hello';
-}
-
-function b() {
-    console.log(this);  // global window
-}
-
-a();
-
-console.log(newvariable);
-
-b();
-
-var c = {
-    name: 'The c object',
-    log: function () {
-        var self = this;
-
-        // this.name = 'Updated c object';
-        self.name = 'Updated c object';
-        // console.log(this);
-        console.log(self);
-
-        var setname = function (newname) {
-            // this.name = newname;
-            self.name = newname;
-            // console.log(this);
-        };
-        setname('Updated again! The c object');
-        // console.log(this);
-        console.log(self);
-    }
-};
-
-c.log();
+(function (name) {
+    var greeting = 'Inside IIFE: Hello';
+    console.log(greeting + ' ' + name);
+}(firstname));      // IIFE
