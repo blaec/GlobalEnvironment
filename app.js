d39b251f08a63a2546eb28f098730f3d1acfa7e2
blaec <odeskonst@yandex.ru>
2020-04-04 15:35:03 +0300

    51. Functional Programming

--- app.js
+++ app.js
@@ -1,46 +1,51 @@
-var person = {
-    firstname: 'John',
-    lastname: 'Doe',
-    getFullName: function () {
-        var fullName = this.firstname + ' ' + this.lastname;
-        return fullName;
-    }
-};
+var arr1 = [1, 2, 3];
+console.log(arr1);
+// _____________________________________________
 
-var logName = function (lang1, lang2) {
-    console.log('Logged: ' + this.getFullName());
-    console.log('Arguments: ' + lang1 + ' ' + lang2);
-    console.log('---------------------------------');
-};
-
-console.log('bind() sample');
-var logPersonName = logName.bind(person);
-logPersonName('en');
-
-console.log('call() sample');
-logName.call(person, 'en', 'es');
-
-console.log('apply() sample');
-logName.apply(person, ['en', 'es']);
-
-
-// function borrowing
-var person2 = {
-    firstname: 'Jane',
-    lastname: 'Doe'
-};
-
-console.log(person.getFullName.apply(person2));
-
-// function curring
-function multiple(a, b) {
-    return a * b;
+var arr2 = [];
+for (var i = 0; i < arr1.length; i++) {
+    arr2.push(arr1[i] * 2);
 }
-var multipleByTwo = multiple.bind(this, 2);
-// function multipleByTwo(b) {
-//     var a = 2;
-//     return a * b;
-// }
-var multipleByThree = multiple.bind(this, 3);
-console.log(multipleByTwo(4));
-console.log(multipleByThree(4));
+console.log(arr2);
+// _____________________________________________
+
+// functional programming
+// _____________________________________________
+
+function mapForEach(arr, fn) {
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+        newArr.push(
+            fn(arr[i])
+        );
+    }
+    return newArr;
+}
+// _____________________________________________
+
+var arr3 = mapForEach(arr1, function (item) {
+    return item * 2;
+});
+console.log(arr3);
+// _____________________________________________
+
+var arr4 = mapForEach(arr1, function (item) {
+    return item > 2;
+});
+console.log(arr4);
+// _____________________________________________
+
+var checkPastLimit = function (limiter, item) {
+    return item > limiter;
+};
+var arr5 = mapForEach(arr1, checkPastLimit.bind(this, 1));
+console.log(arr5);
+// _____________________________________________
+
+var checkPastLimitSimplified = function (limiter) {
+    return function (limiter, item) {
+        return item > limiter;
+    }.bind(this, limiter);
+};
+var arr6 = mapForEach(arr1, checkPastLimitSimplified(1));
+console.log(arr6);
