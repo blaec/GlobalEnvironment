0a039982813434885361a51b09689d7356e3c525
blaec <odeskonst@yandex.ru>
2020-04-04 13:04:56 +0300

    48. Framework Aside: Function Factories

--- app.js
+++ app.js
@@ -1,76 +1,16 @@
-function greet(whattosay) {
-    return function (name) {
-        console.log(whattosay + ' ' + name);
-    };
-}
-
-greet('Hi')('Tony');
-
-var sayHi = greet('Hi');
-sayHi('Tony');
-
-console.log('-- end part 1 ---');
-
-function buildFunctions() {
-    var arr = [];
-    for (var i = 0; i < 3; i++) {
-        arr.push(
-            function () {
-                console.log(i);
-            }
-        )
+function makeGreeting(language) {
+    return function (firstname, lastname) {
+        if (language === 'en') {
+            console.log('Hello ' + firstname + ' ' + lastname);
+        }
+        if (language === 'es') {
+            console.log('Hola ' + firstname + ' ' + lastname);
+        }
     }
-
-    return arr;
 }
 
-var fs = buildFunctions();
+var greetEnglish = makeGreeting('en');
+var greetSpanish = makeGreeting('es');
 
-fs[0]();
-fs[1]();
-fs[2]();
-
-console.log('-- end part 2.1 ---');
-
-function buildFunctions2() {
-    var arr = [];
-    for (var i = 0; i < 3; i++) {
-        let j = i;
-        arr.push(
-            function () {
-                console.log(j);
-            }
-        )
-    }
-
-    return arr;
-}
-
-var fs2 = buildFunctions2();
-
-fs2[0]();
-fs2[1]();
-fs2[2]();
-
-console.log('-- end part 2.2 [es6]---');
-
-function buildFunctions3() {
-    var arr = [];
-    for (var i = 0; i < 3; i++) {
-        arr.push(
-            (function (j) {
-                console.log(j);
-            }(i))
-        )
-    }
-
-    return arr;
-}
-
-var fs3 = buildFunctions3();
-
-fs3[0];
-fs3[1];
-fs3[2];
-
-console.log('-- end part 2.3 [es5]---');
+greetEnglish('John', 'Doe');
+greetSpanish('John', 'Doe');
