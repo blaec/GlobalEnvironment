b5f4b9498de7527ae3aaa6bfa20a6a013047a024
blaec <odeskonst@yandex.ru>
2020-04-06 20:03:33 +0300

    57.1 Function Constructors, 'new', and the History of Javascript

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-function Person() {
+function Person(firstname, lastname) {
     console.log(this);
-    this.firstname = 'John';
-    this.lastname = 'Doe';
+    this.firstname = firstname;
+    this.lastname = lastname;
 }
 
 // if function invoked with 'new' word - this pointed to Person object
-var john = new Person();
+var john = new Person('John', 'Doe');
 console.log(john);
+
+var jane = new Person('Jane', 'Doe');
+console.log(jane);
