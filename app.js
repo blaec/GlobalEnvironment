4e5d4ab7ba16dd1c2d9792b129e284d58bc69593
blaec <odeskonst@yandex.ru>
2020-04-06 19:59:35 +0300

    57. Function Constructors, 'new', and the History of Javascript

--- app.js
+++ app.js
@@ -1,40 +1,9 @@
-var person = {
-    firstname: 'Default',
-    lastname: 'Default',
-    getFullName: function () {
-        return this.firstname + ' ' + this.lastname
-    }
-};
-
-var john = {
-    firstname: 'John',
-    lastname: 'Doe'
-};
-
-// don't do this EVER! for demo purpose only!!!
-john.__proto__ = person;
-
-for (var prop in john) {
-    if (john.hasOwnProperty(prop)) {
-        console.log(prop + ': ' + john[prop]);
-    }
+function Person() {
+    console.log(this);
+    this.firstname = 'John';
+    this.lastname = 'Doe';
 }
 
-var jane = {
-    firstname: 'Jane',
-    address: '111 Main St.',
-    getFormalFullName: function () {
-        return this.lastname + ', ' + this.firstname;
-    }
-};
-
-var jim = {
-    firstname: 'Jim',
-    getFirstName: function () {
-        return firstname;
-    }
-};
-
-_.extend(john, jane, jim);
-
+// if function invoked with 'new' word - this pointed to Person object
+var john = new Person();
 console.log(john);
