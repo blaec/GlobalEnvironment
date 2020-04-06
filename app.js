2ac64d747d3301cc4fbf0ecd25d0dc6d2544efd6
blaec <odeskonst@yandex.ru>
2020-04-06 22:00:20 +0300

    58. Function Constructors and '.prototype'

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ function Person(firstname, lastname) {
     this.lastname = lastname;
 }
 
+Person.prototype.getFullName = function () {
+    return this.firstname + ' ' + this.lastname;
+};
+
 // if function invoked with 'new' word - this pointed to Person object
 var john = new Person('John', 'Doe');
 console.log(john);
 
 var jane = new Person('Jane', 'Doe');
 console.log(jane);
+
+Person.prototype.getFormalFullName = function () {
+    return this.lastname + ', ' + this.firstname;
+};
